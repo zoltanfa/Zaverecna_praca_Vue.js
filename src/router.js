5a54679b486b8231ec2d6f28e5b80c809e8ce8fb
Zoltan Fazekas <zoltan.fazekas2003@gmail.com>
2026-02-24 15:36:23 +0100

    github pages deploy

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Products from '@/views/Products.vue'
 import ProductDetail from '@/views/ProductDetail.vue'
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
