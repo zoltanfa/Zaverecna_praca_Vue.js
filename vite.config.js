8c3dd617698d9877d762478442c057aae1fa894b
Zoltan Fazekas <zoltan.fazekas2003@gmail.com>
2026-02-24 15:02:15 +0100

    vite change

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/Zaverecna_praca_Vue.js/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
